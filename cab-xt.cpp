d83877e0ae5b5733379013d579dce8fb921ffde0
Eduardo Costa <m4c0@users.noreply.github.com>
2022-12-25 19:34:50 +0000

    somewhat reusable cab extract

--- cab-xt.cpp
+++ cab-xt.cpp
@@ -5,37 +5,6 @@
 
 import cab;
 
-const auto &find_file(const cab::cab &c, const std::string &fn) {
-  for (auto i = 0U; i < c.hdr.num_files; i++) {
-    const auto &fl = c.files[i];
-    if (fl.name != fn)
-      continue;
-
-    return fl;
-  }
-
-  throw std::runtime_error("Could not find target file");
-}
-void xt(const cab::cab &c, const std::string &fn) {
-  const auto &file = find_file(c, fn);
-  const auto &fld = c.folders[file.folder];
-  if (fld.compress != 1)
-    throw std::runtime_error("Only MSZIP is supported");
-
-  cab::folder_deflater fdef{fld};
-  for (auto i = 0U; i < file.unc_offset; i++) {
-    if (!fdef.next())
-      throw std::runtime_error("Folder data is smaller than file offset");
-  }
-
-  for (int i = 0; i < file.unc_size; i++) {
-    auto n = fdef.next();
-    if (!n)
-      throw std::runtime_error("Failed to read stuff");
-    std::cout << (char)*n;
-  }
-}
-
 void try_main(int argc, char **argv) {
   if (argc != 3)
     throw std::runtime_error("Usage: cab-xt <cab> <file-to-extract>");
@@ -44,7 +13,7 @@ void try_main(int argc, char **argv) {
   if (!f)
     throw std::runtime_error("Could not open input file");
 
-  xt(cab::read(f.rdbuf()), argv[2]);
+  cab::extract(std::cout, f.rdbuf(), argv[2]);
 }
 
 int main(int argc, char **argv) {
--- cab.cppm
+++ cab.cppm
@@ -1,4 +1,47 @@
+module;
+#include <optional>
+#include <ostream>
+#include <string>
+
 export module cab;
 export import :pods;
 export import :reader;
 export import :deflater;
+
+namespace cab {
+export const auto &find_file(const cab &c, const std::string &fn) {
+  for (auto i = 0U; i < c.hdr.num_files; i++) {
+    const auto &fl = c.files[i];
+    if (fl.name != fn)
+      continue;
+
+    return fl;
+  }
+
+  throw std::runtime_error("Could not find target file");
+}
+export void extract(std::ostream &out, const cab &c, const std::string &fn) {
+  const auto &file = find_file(c, fn);
+  const auto &fld = c.folders[file.folder];
+  if (fld.compress != 1)
+    throw std::runtime_error("Only MSZIP is supported");
+
+  folder_deflater fdef{fld};
+  for (auto i = 0U; i < file.unc_offset; i++) {
+    if (!fdef.next())
+      throw std::runtime_error("Folder data is smaller than file offset");
+  }
+
+  for (int i = 0; i < file.unc_size; i++) {
+    auto n = fdef.next();
+    if (!n)
+      throw std::runtime_error("Failed to read stuff");
+    out << (char)*n;
+  }
+}
+export void extract(std::ostream &out, std::streambuf *rdbuf,
+                    const std::string &fn) {
+  extract(out, read(rdbuf), fn);
+}
+
+} // namespace cab
